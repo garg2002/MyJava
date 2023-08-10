4a93b99bda08473620b21b3322fa11697dc8c68d
Satyam Garg <satyamgarg1308@gmail.com>
2023-08-10 12:33:53 +0530

    Literals

---
+++ Literals.java
@@ -0,0 +1,30 @@
+/*
+Literals are the constant values that can be assigned to the variables.
+
+There are five types of literals in Java:
+1. Integer literals
+2. Floating point literals
+3. Character literals
+4. String literals
+5. Boolean literals
+*/
+
+public class Literals {
+    public static void main(String[] args) {
+        // Integer literals
+        int i = 10;
+        int j = 0b1010;
+        int k = 012;
+        int l = 0xA;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+
+        //Floating point literals
+        float f = 10.5f;
+        double d = 10.5;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
