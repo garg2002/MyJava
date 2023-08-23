2667be6ae480fb0d0c0b255baaf0725fba543de0
Satyam Garg <satyamgarg1308@gmail.com>
2023-08-23 23:19:29 +0530

    AverageUsing Command Line Arguments

---
+++ Average.java
@@ -0,0 +1,15 @@
+import java.lang.Integer;
+public class Average {
+    public static void main(String args[]) {
+    
+    int sum = 0;
+     
+    for(int i=0;i<args.length;i++)
+    {
+        sum +=Integer.parseInt(args[i]);
+    }
+    int average = sum / args.length;
+    
+    System.out.println("Average of " + args.length + " command line arguments is " + average);
+    }
+}
