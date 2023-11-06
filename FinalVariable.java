ae29c45d0f3cfe455d8b83d7fc8b7b1140cc158f
Satyam Garg <113013930+garg2002@users.noreply.github.com>
2023-11-06 19:28:39 +0530

    Final Variable in Java

---
+++ FinalVariable.java
@@ -0,0 +1,16 @@
+/**
+ * FinalVariable:- 
+ final variables are used to declare constants in Java programming language, which means that once the value of this constant is assigned during its declaration or
+ */
+public class FinalVariable {
+
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println(a);
+        //final variable can't be reassigned after initialization
+        //  a = 20;
+
+        // System.out.println(a);
+        
+    }
+}
