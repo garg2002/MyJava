3a950ab8b878b809f95113741a98d6afa3024bb0
Satyam Garg <113013930+garg2002@users.noreply.github.com>
2023-11-15 21:24:55 +0530

    ArrayList in java

---
+++ ArrayList1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayList1 {
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    // Add elements to the array list
+    
+    cars.add("Toyota");
+    cars.add("Honda");
+    cars.add("Chevrolet");
+    cars.add("Nissan");
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
